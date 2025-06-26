e141c7f39a2815c668008a6a63dc5ec414360d21
hiepta <158310799+hiepta@users.noreply.github.com>
2025-06-26 23:22:06 +0700

    Update employee add form

--- src/components/employee/Add.jsx
+++ src/components/employee/Add.jsx
@@ -1,27 +1,32 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { fetchDepartments } from '../../utils/EmployeeHelper'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+
 const Add = () => {
-    const [departments, setDepartments] = useState([])
-    const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        role: "",
+        profileImage: null,
+        fullName: "",
+        gender: "",
+        dob: "",
+        permanentAddress: "",
+        phoneNumber: "",
+        personalEmail: "",
+        idNumber: "",
+        idIssuedDate: "",
+        idIssuedPlace: ""
+    })
     const navigate = useNavigate()
 
-    useEffect(() => {
-
-        const getDepartments = async () => {
-        const departments = await fetchDepartments()
-        setDepartments(departments)
-        }
-        getDepartments()
-    }, [])
-
     const handleChange = (e) => {
-        const {name, value, files} = e.target;
-        if(name === 'image'){
-            setFormData((prevData) => ({...prevData, [name] : files[0]}))
-        }else{
-            setFormData((prevData) => ({...prevData, [name] : value}))
+        const { name, value, files } = e.target
+        if (name === 'profileImage') {
+            setFormData((prevData) => ({ ...prevData, [name]: files[0] }))
+        } else {
+            setFormData((prevData) => ({ ...prevData, [name]: value }))
         }
     }
     
@@ -51,91 +56,73 @@ const Add = () => {
         <div className='max-w-4xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md'>
       <h2 className='text-2xl font-bold mb-6 text-black'>Thêm mới nhân viên</h2>
       <form onSubmit={handleSubmit}>
-        {/* Name */}
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             <div>
-                <label className='block text-sm font-medium text-gray-700'>Tên nhân viên</label>
-                <input onChange={handleChange} type="text" name='name' placeholder='Insert Name' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
+                <label className='block text-sm font-medium text-gray-700'>Tên đăng nhập</label>
+                <input onChange={handleChange} type="text" name='name' placeholder='Name' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
             </div>
-        
-
-        {/* Email */}
             <div>
                 <label className='block text-sm font-medium text-gray-700'>Email</label>
-                <input type="email" onChange={handleChange} name='email' placeholder='Insert Email' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
+                <input type="email" onChange={handleChange} name='email' placeholder='Email' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
             </div>
-
-        {/* Employee */}
             <div>
-                <label className='block text-sm font-medium text-gray-700'>Mã nhân viên</label>
-                <input type="text" onChange={handleChange} name='employeeId' placeholder='Employee ID' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
+                <label className='block text-sm font-medium text-gray-700'>Mật khẩu</label>
+                <input type="password" onChange={handleChange} name='password' placeholder='******' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
             </div>
-
-        {/* Date of birth */}
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Vai trò</label>
+                <select name='role' onChange={handleChange} className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'>
+                    <option value="">Lựa chọn vai trò</option>
+                    <option value="admin">Admin</option>
+                    <option value="employee">Nhân viên</option>
+                </select>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Họ và tên</label>
+                <input type="text" onChange={handleChange} name='fullName' placeholder='Full Name' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
             <div>
                 <label className='block text-sm font-medium text-gray-700'>Ngày sinh</label>
-                <input type="date" onChange={handleChange} name='dob' placeholder='DOB' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
+                <input type="date" onChange={handleChange} name='dob' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
             </div>
-
-        {/* Gender */}
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Giới tính</label>
-            <select name='gender' onChange={handleChange} className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'>
-                <option value="">Giới tính</option>
-                <option value="male">Nam</option>
-                <option value="female">Nữ</option>
-                <option value="other">Khác</option>
-            </select>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Giới tính</label>
+                <select name='gender' onChange={handleChange} className='mt-1 p-2 block w-full border border-gray-300 rounded-md'>
+                    <option value="">Giới tính</option>
+                    <option value="male">Nam</option>
+                    <option value="female">Nữ</option>
+                    <option value="other">Khác</option>
+                </select>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Email cá nhân</label>
+                <input type="email" onChange={handleChange} name='personalEmail' placeholder='Personal Email' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Số điện thoại</label>
+                <input type="text" onChange={handleChange} name='phoneNumber' placeholder='Phone Number' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Địa chỉ thường trú</label>
+                <input type="text" onChange={handleChange} name='permanentAddress' placeholder='Permanent Address' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Số CMND/CCCD</label>
+                <input type="text" onChange={handleChange} name='idNumber' placeholder='ID Number' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Ngày cấp</label>
+                <input type="date" onChange={handleChange} name='idIssuedDate' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Nơi cấp</label>
+                <input type="text" onChange={handleChange} name='idIssuedPlace' placeholder='Issued Place' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700'>Ảnh đại diện</label>
+                <input type="file" name='profileImage' onChange={handleChange} accept='image/*' className='mt-1 p-2 block w-full border border-gray-300 rounded-md'/>
+            </div>
         </div>
-        
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Tình trạng hôn nhân</label>
-            <select name='maritalStatus' onChange={handleChange} placeholder='Marital Status' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'>
-                <option value="">Tình trạng hôn nhân</option>
-                <option value="single">Độc thân</option>
-                <option value="married">Đã kết hôn</option>
-            </select>
-        </div>
-
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Designation</label>
-            <input type="text" onChange={handleChange} name='designation' placeholder='Designation' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
-        </div>
-
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Phòng ban</label>
-            <select name='department' onChange={handleChange} className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'>
-                <option value="">Lựa chọn phòng ban</option>
-                {departments.map((dep) =>(
-                    <option key={dep._id} value={dep._id}>{dep.dep_name}</option>
-                ))}
-            </select>
-        </div>
-
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Lương</label>
-            <input type="number" onChange={handleChange} name='salary' placeholder='Salary' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
-        </div>
-
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Mật khẩu</label>
-            <input type="password" onChange={handleChange} name='password' placeholder='******' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
-        </div>
-
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Vai trò</label>
-            <select name='role' onChange={handleChange} className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'>
-                <option value="">Lựa chọn vai trò</option>
-                <option value="admin">Admin</option>
-                <option value="employee">Nhân viên</option>
-            </select>
-        </div>
-
-        <div>
-            <label className='block text-sm font-medium text-gray-700'>Ảnh chân dung</label>
-            <input type="file" name='image' onChange={handleChange} placeholder='Upload Image' accept='image/*' className='mt-1 p-2 block w-full border border-gray-300 rounded-md required'/>
-        </div>
-    </div>
         <button type='submit' className='w-full mt-6 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded required'>
             Thêm nhân viên
         </button>
